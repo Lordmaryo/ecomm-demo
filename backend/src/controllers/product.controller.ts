9cedb7598304789385f67139744eca4793146eab
Lordmaryo <emmanuelofoneta@gmail.com>
2025-01-02 01:36:02 +0100

    Connect to cloudinary Api

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,6 +1,7 @@
 import { Request, RequestHandler, Response } from "express";
 import Product from "../models/product.model";
 import { redis } from "../lib/redis";
+import cloudinary from "../lib/cloudinary";
 
 export const getAllProducts: RequestHandler = async (
   req: Request,
@@ -46,5 +47,70 @@ export const getFeaturedProducts: RequestHandler = async (
       return;
     }
     res.status(500).json({ mesage: "Internal server error" });
+  }
+};
+
+export const createProduct: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { name, description, image, category, price, isFeatured } = req.body;
+    let cloudinaryResponse = null;
+
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+
+    const product = Product.create({
+      name,
+      description,
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+      price,
+      isFeatured,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+//@ts-ignore
+export const deleteProduct: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    if (product.image) {
+      const publicId = product.image.split("/").pop()?.split(".")[0];
+      try {
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: `Error uploading image`, error: error });
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    }
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+  createProduct,
+  deleteProduct
+} from "../controllers/product.controller";
 import { adminRoute, protectedRoute } from "../middleware/auth.middleware";
 
 const productRoutes = express.Router();
 
 productRoutes.get("/", protectedRoute, adminRoute, getAllProducts);
 productRoutes.get("/featured", getFeaturedProducts);
+productRoutes.post("/", protectedRoute, adminRoute, createProduct);
+productRoutes.delete("/delete/:id", protectedRoute, adminRoute, deleteProduct);
 
 export default productRoutes;
