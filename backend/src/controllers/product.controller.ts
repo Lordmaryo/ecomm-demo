c8c071e02af6b50887a1be34e837443aff6008c9
Lordmaryo <emmanuelofoneta@gmail.com>
2025-01-02 17:34:31 +0100

    Finalize products routes

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -101,16 +101,97 @@ export const deleteProduct: RequestHandler = async (
       const publicId = product.image.split("/").pop()?.split(".")[0];
       try {
         await cloudinary.uploader.destroy(`products/${publicId}`);
+        console.log("Image deleted from cloudinary");
       } catch (error) {
         return res
           .status(500)
           .json({ message: `Error uploading image`, error: error });
       }
     }
+
+    await Product.findByIdAndDelete(id);
+    return res.status(204).json({ message: "Product deleted successfully" });
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     }
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getRecommendedProducts: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const products = await Product.aggregate([
+      { $sample: { size: 4 } },
+      { $project: { _id: 1, name: 1, description: 1, image: 1, price: 1 } },
+    ]);
+
+    res.json(products);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getProductsByCategory: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { category } = req.params;
+    const products = await Product.find({ category });
+    res.json(products);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+      return;
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const toggleFeauturedProduct: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+
+    if (product) {
+      product.isFeatured = !product.isFeatured;
+      const updatedproduct = await product.save();
+      await updateFeaturedProductCache();
+      res.json(updatedproduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+      return;
+    }
+    res
+      .status(500)
+      .json({ message: "Internal server error: toggleFeauturedProduct" });
+  }
+};
+
+const updateFeaturedProductCache = async () => {
+  try {
+    const featuredProducts = await Product.find({ isFeatured: true }).lean();
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("Internal server error: updateFeaturedProductCache error");
+    }
   }
 };
--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -3,7 +3,10 @@ import {
   getAllProducts,
   getFeaturedProducts,
   createProduct,
-  deleteProduct
+  deleteProduct,
+  getRecommendedProducts,
+  getProductsByCategory,
+  toggleFeauturedProduct,
 } from "../controllers/product.controller";
 import { adminRoute, protectedRoute } from "../middleware/auth.middleware";
 
@@ -11,7 +14,15 @@ const productRoutes = express.Router();
 
 productRoutes.get("/", protectedRoute, adminRoute, getAllProducts);
 productRoutes.get("/featured", getFeaturedProducts);
+productRoutes.get("/recommendation", getRecommendedProducts);
+productRoutes.get("/category/:category", getProductsByCategory);
 productRoutes.post("/", protectedRoute, adminRoute, createProduct);
+productRoutes.patch(
+  "/update_feature/:id",
+  protectedRoute,
+  adminRoute,
+  toggleFeauturedProduct
+);
 productRoutes.delete("/delete/:id", protectedRoute, adminRoute, deleteProduct);
 
 export default productRoutes;
