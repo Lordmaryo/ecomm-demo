b0fa362b7154ed4952f2223fe1b70fcc5e9d4d35
Lordmaryo <emmanuelofoneta@gmail.com>
2025-01-15 11:54:26 +0100

    finalize project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import authRoutes from "./routes/auth.route";
 import productRoutes from "./routes/product.route";
@@ -8,9 +9,12 @@ import couponRoutes from "./routes/coupon.route";
 import paymentRoutes from "./routes/payment.route";
 import analyticsRoutes from "./routes/analytics.route";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -27,6 +31,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupon", couponRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
